c366bfaac62b6a651ef609ae2d5028d8af62b33c
xoan <xoancastrogarcia@gmail.com>
2021-04-10 17:12:42 +0200

    Minor fixes Activity4 layout

--- app/src/main/java/com/ilerna/pac_pmdm/Activity4.kt
+++ app/src/main/java/com/ilerna/pac_pmdm/Activity4.kt
@@ -2,10 +2,17 @@ package com.ilerna.pac_pmdm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Activity4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_4)
+
+        // Activity1 back button
+        val btnBack = findViewById<Button>(R.id.btnBacktoA1)
+        btnBack.setOnClickListener {
+            finish()
+        }
     }
 }
